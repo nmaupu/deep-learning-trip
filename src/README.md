74c9e505a8522686d858448b94e8d6893022ce03
Nicolas Maupu <nmaupu@gmail.com>
2024-05-26 21:08:13 +0200

    Refacto

---
+++ src/README.md
@@ -0,0 +1,1 @@
+Based on https://www.youtube.com/watch?v=XUFLq6dKQok&list=PLO_fdPEVlfKoanjvTJbIbd9V5d9Pzp8Rw
--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
+from model import LearningModel
 from neuron import Neuron
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,22 +8,26 @@ if __name__ == "__main__":
     X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
     y = y.reshape((y.shape[0], 1))
 
-    n = Neuron(X, y)
-    n.train(n_iter=100, learning_rate=0.1)
+    model = LearningModel(X, y)
+    W, b = model.train(n_iter=100, learning_rate=0.1)
 
     print("Neuron has been trained.")
-    print("W=", n.W)
-    print("b=", n.b)
-    print("accuracy score=", n.accuracy)
+    print("W=", W)
+    print("b=", b)
+    print("accuracy score=", model.accuracy)
 
     new_plant = np.array([2, 1])
 
     x0 = np.linspace(-1, 4, 100)
-    x1 = (-n.W[0] * x0 - n.b) / n.W[1]
+    x1 = (-W[0] * x0 - b) / W[1]
 
-    plt.scatter(n.X[:, 0], n.X[:, 1], c=n.y, cmap="summer")
+    plt.plot(model.loss)
+    plt.show()
+
+    plt.scatter(model.X[:, 0], model.X[:, 1], c=model.y, cmap="summer")
     plt.scatter(new_plant[0], new_plant[1], c="red")
     plt.plot(x0, x1, c="orange")
     plt.show()
 
-    print(n.predict(new_plant))
+    neuron = Neuron(W, b)
+    print(neuron.predict(new_plant))
